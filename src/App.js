69c75862d56e1e3a931f1a83dd17b40eba1d3762
kimsehyun <shkim_0125@naver.com>
2024-09-28 23:35:34 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,169 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import FeedPage from './Pages/RegisterPage/2nd_hw/FeedPage';
+
+
 
 function App() {
+  const posts = [
+    { title: "게으르다고 쫒겨났다.", subtitle: "회사에서 게으르다고 쫒겨났다.", date: "2024년 08월 14일", comments: "23개의 댓글", nick: "살몬" },
+    { title: "다시 시작하기로 했다.", subtitle: "새로운 도전을 시작했다.", date: "2024년 08월 15일", comments: "10개의 댓글", nick: "이사벨" },
+    { title: "여름의 끝을 느끼다.", subtitle: "여름의 마지막 날을 즐기다.", date: "2024년 08월 16일", comments: "15개의 댓글", nick: "제이크" },
+    { title: "여행의 기억", subtitle: "지난 여행에 대한 이야기.", date: "2024년 08월 17일", comments: "5개의 댓글", nick: "애나" },
+    { title: "커피 한 잔의 여유", subtitle: "좋은 커피와 함께한 오후.", date: "2024년 08월 18일", comments: "8개의 댓글", nick: "존" },
+    { title: "가을이 오고 있다.", subtitle: "가을의 준비를 하다.", date: "2024년 08월 19일", comments: "18개의 댓글", nick: "소피" },
+  ];
+
+  const heartCounts = [46, 20, 30, 12, 22, 15];
+
   return (
-    <BaseContainer>
-      <Container>
-        <WelcomeStc>환영합니다!</WelcomeStc>
-        <ResisterStc>기본 회원 정보를 등록해주세요</ResisterStc>
-
-        <ItemBox>
-          <Label>이름</Label>
-          <Input type="text" placeholder="이름을 입력해주세요." width="249px" />
-        </ItemBox>
-
-        <ItemBox>
-          <Label>이메일</Label>
-          <Input type="email" placeholder="이메일을 입력해주세요." width="333px" />
-        </ItemBox>
-
-        <ItemBox>
-          <Label>비밀번호</Label>
-          <Input type="password" placeholder="비밀번호를 입력해주세요." width="274px" />
-        </ItemBox>
-
-        <ItemBox>
-          <Label>한 줄 소개</Label>
-          <Input type="text" placeholder="당신을 한 줄로 소개해보세요" width="373px" />
-        </ItemBox>
-
-        <Stc>
-          <input type='checkbox' className='CheckBox' />
-          <Url href='#'>이용약관</Url>과 <Url href='#'>개인정보취급방침</Url>에 동의합니다.
-        </Stc>
-
-        <ButtonsBox>
-          <Btn1>취소</Btn1>
-          <Btn2>가입</Btn2>
-        </ButtonsBox>
-      </Container>
-    </BaseContainer>
+    <div>
+      <FeedPage initialPosts={posts} initialHeartCounts={heartCounts} />
+      {/* <RegisterPage /> */}
+      {/* <Pangil /> */}
+    </div>
   );
 }
-
-const BaseContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #ffffff;
-`;
-
-const Container = styled.div`
-  background: white;
-  padding: 0;
-  width: 373px;
-`;
-
-const WelcomeStc = styled.h1`
-  margin-top: 168px;
-  margin-bottom: 0;
-  text-align: left;
-  font-family: Inter;
-  font-size: 51px;
-  font-weight: 700;
-  line-height: 61.72px;
-  color: #212529;
-`;
-
-const ResisterStc = styled.p`
-  margin-top: 14px;
-  margin-bottom: 46px;
-  text-align: left;
-  font-family: Inter;
-  font-size: 19px;
-  font-weight: 400;
-  line-height: 22.99px;
-  color: #000;
-`;
-
-const ItemBox = styled.div`
-  margin-bottom: 23.5px;
-`;
-
-const Label = styled.p`
-  font-family: Inter;
-  margin-bottom: 20px;
-  margin-top: 0;
-  font-size: 15px;
-  font-weight: 700;
-  text-align: left;
-  color: #ACB5BD;
-`;
-
-const Input = styled.input`
-  width: ${(props) => props.width};
-  height: 33.5px;
-  border: none;
-  border-bottom: 1px solid #ACB5BD;
-  outline: none;
-  font-family: Inter;
-  font-size: 19px;
-  font-weight: 400;
-  line-height: 22.99px;
-  color: #ACB5BD;
-
-  &::placeholder {
-    color: #ACB5BD;
-    opacity: 1;
-    font-weight: 400;
-    font-family: Inter;
-    font-size: 19px;
-    line-height: 22.99px;
-  }
-`;
-
-const Stc = styled.p`
-  font-family: Inter;
-  font-size: 13px;
-  font-weight: 400;
-  line-height: 15.73px;
-  text-align: left;
-  color: #000;
-  margin-bottom: 84px;
-`;
-
-const Url = styled.a`
-  color: #15B886;
-  font-family: Inter;
-  font-size: 13px;
-  font-weight: 400;
-  line-height: 15.73px;
-`;
-
-const ButtonsBox = styled.div`
-  display: flex;
-  gap: 10px;
-`;
-
-const Btn1 = styled.div`
-  width: 93px;
-  height: 41px;
-  border-radius: 20.5px;
-  background: #DEE2E6;
-  font-family: Inter;
-  font-size: 20px;
-  font-weight: 700;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #000;
-`;
-
-const Btn2 = styled.div`
-  width: 93px;
-  height: 41px;
-  border-radius: 20.5px;
-  background: #15B886;
-  font-family: Inter;
-  font-size: 20px;
-  font-weight: 700;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #fff;
-`;
 
 export default App;
